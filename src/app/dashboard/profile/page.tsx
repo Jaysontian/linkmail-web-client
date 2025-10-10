b1c7cec1e3e19b65e6af8139b0d0babcfd454bed
Ishaan Gupta <ishaan4g@gmail.com>
2025-10-10 13:01:28 -0700

    remove 100% widget

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -270,28 +270,30 @@ export default function ProfilePage() {
             </p>
           </div>
           
-          <div className="ml-8 bg-yellow-50 border border-yellow-200 rounded-lg p-4 w-[400px]">
-            <div className="flex items-start">
-              <div className="flex-shrink-0">
-                <svg className="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-                  <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-              </div>
-              <div className="ml-3 flex-1">
-                <h4 className="text-sm font-medium text-yellow-800">
-                  Complete Your Profile
-                </h4>
-                <div className="mt-2 text-sm text-yellow-700">
-                  <p>Your profile is {setupStatus.setupPercentage}% complete. Please add the following information to get started:</p>
-                  <ul className="mt-2 list-disc list-inside">
-                    {setupStatus.missingFields.map((field, index) => (
-                      <li key={index}>{field}</li>
-                    ))}
-                  </ul>
+          {setupStatus.setupPercentage < 100 && (
+            <div className="ml-8 bg-yellow-50 border border-yellow-200 rounded-lg p-4 w-[400px]">
+              <div className="flex items-start">
+                <div className="flex-shrink-0">
+                  <svg className="h-5 w-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
+                    <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <div className="ml-3 flex-1">
+                  <h4 className="text-sm font-medium text-yellow-800">
+                    Complete Your Profile
+                  </h4>
+                  <div className="mt-2 text-sm text-yellow-700">
+                    <p>Your profile is {setupStatus.setupPercentage}% complete. Please add the following information to get started:</p>
+                    <ul className="mt-2 list-disc list-inside">
+                      {setupStatus.missingFields.map((field, index) => (
+                        <li key={index}>{field}</li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
 
